f2ac9e8f5d24a1c3da3d3655ff03b0c6bd1a89f1
Gurpreet Deol <deol.gs@hotmail.com>
2018-12-19 08:13:56 -0800

    pages and services creation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './pages/authentication/authentication.component';
+import { ChatListComponent } from './pages/chat-list/chat-list.component';
+import { ConversationComponent } from './pages/conversation/conversation.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    ChatListComponent,
+    ConversationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
